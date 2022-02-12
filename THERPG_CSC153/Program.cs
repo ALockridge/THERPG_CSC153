5bf755fe8e590add3e4bb30e488c3f9befcc2f55
ajlstick <somecrazystickfig04@gmail.com>
2022-02-12 10:39:45 -0500

    Add project files.

---
+++ THERPG_CSC153/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace THERPG_CSC153
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			bool exit = false;
+
+			//Main menu
+
+			while (exit == false)
+			{
+
+				Console.WriteLine("TODO - User prompt");
+				Console.WriteLine("1. A User's Choice");
+				Console.WriteLine("2. Exit");
+				Console.Write("Enter a choice > ");
+				string input = Console.ReadLine();
+
+				if (input == "1")
+				{
+					//TODO
+				}
+				else if (input == "2")
+				{
+					exit = true;
+				}
+				else
+				{
+					Console.WriteLine("Not a choice!");
+				}
+			}
+
+		}
+    }
+}
